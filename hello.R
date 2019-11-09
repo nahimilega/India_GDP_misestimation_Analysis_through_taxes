82982962e75386d1ebf38f76127d62d23b246a21
Archit Agrawal <archit18221@iiitd.ac.in>
2019-11-09 22:15:06 +0530

    Add correlations to check Omitted variable bias

--- hello.R
+++ hello.R
@@ -46,14 +46,24 @@ cou<-data1$Country[data1$Country %in% countries]
 
 linear1<-function(countries,data,year)
 {
+
+  # Linear regression without tax
   data2<-(data[data$Country %in% countries & data$T == year,])
   model=lm(GDP.growth..annual...~Domestic.credit.to.private.sector....of.GDP.+Exports.of.goods.and.services..annual...growth.+Imports.of.goods.and.services..annual...growth.+India_Dummy,data = data2)
 
   print(summary(model))
+  print("Correlation b/w TAX and Y")
+  print(cor(data2$Tax.revenue....of.GDP. ,data2$GDP.growth..annual...))
+  print("Correlation b/w TAX and Other parms")
+  print(cor(data2$Tax.revenue....of.GDP. ,data2$Exports.of.goods.and.services..annual...growth.))
+  print(cor(data2$Tax.revenue....of.GDP. ,data2$Domestic.credit.to.private.sector....of.GDP.))
+  print(cor(data2$Tax.revenue....of.GDP. ,data2$Imports.of.goods.and.services..annual...growth.))
+  
 }
 
 linear2<-function(countries,data,year)
 {
+  # Linear regression with tax
   data2<-(data[data$Country %in% countries & data$T == year,])
   model=lm(GDP.growth..annual...~Domestic.credit.to.private.sector....of.GDP.+Exports.of.goods.and.services..annual...growth.+Imports.of.goods.and.services..annual...growth.+India_Dummy+Tax.revenue....of.GDP.,data = data2)
   x1<-as.matrix(data.frame(1,data2[data2$Country == "India",]$Domestic.credit.to.private.sector....of.GDP.,data2[data2$Country == "India",]$Exports.of.goods.and.services..annual...growth.,data2[data2$Country == "India",]$Imports.of.goods.and.services..annual...growth.,data2[data2$Country == "India",]$India_Dummy,data2[data2$Country == "India",]$Tax.revenue....of.GDP.))
